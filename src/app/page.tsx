30feab12cb4ca139861808947631c170c94918d9
Alex M <contact@stkyfngrs.com>
2024-11-01 20:37:18 -0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react"
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
@@ -10,6 +12,7 @@ export default function Home() {
             </text>
           </g>
         </svg>
+        <Analytics/>
       </main>
       <footer></footer>
     </div>
