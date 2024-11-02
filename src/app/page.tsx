e66ffc43ac94aead86d5227f96d612677f047447
Alex M <contact@stkyfngrs.com>
2024-11-01 18:47:07 -0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100">
-      <g transform="translate(40, 20)">
-      <rect x="0" y="0" width="120" height="50" fill="none" stroke="#000000" stroke-width="2"/>
-      <text x="60" y="40" font-family="Arial" font-weight="900" font-size="40" text-anchor="middle">
-      <tspan fill="#000000">DUDE</tspan>
-      </text>
-      </g>
-      </svg>
-
-        
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100">
+          <g transform="translate(40, 20)">
+            <rect x="0" y="0" width="120" height="50" fill="none" stroke="#FFFFFF" strokeWidth="2"/>
+            <text x="60" y="40" fontFamily="Arial" fontWeight="900" fontSize="40" textAnchor="middle">
+              <tspan fill="#FFFFFF">DUDE</tspan>
+            </text>
+          </g>
+        </svg>
       </main>
       <footer></footer>
     </div>
