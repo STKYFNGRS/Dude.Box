5b8bb5a161e00d3675d0a1d1f252dacfc7837741
Alex M <contact@stkyfngrs.com>
2024-11-01 19:20:44 -0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100" width="200" height="100">
           <g transform="translate(40, 20)">
             <rect x="0" y="0" width="120" height="50" fill="none" stroke="#FFFFFF" strokeWidth="2"/>
             <text x="60" y="40" fontFamily="Arial" fontWeight="900" fontSize="40" textAnchor="middle">
