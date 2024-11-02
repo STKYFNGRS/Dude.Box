d46f06beb7974f73b3eb0101e31d7e7d4dcf211f
Alex M <contact@stkyfngrs.com>
2024-11-01 18:42:47 -0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -6,13 +5,13 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100">
-  <g transform="translate(40, 20)">
-    <rect x="0" y="0" width="120" height="50" fill="none" stroke="#000000" stroke-width="2"/>
-    <text x="60" y="40" font-family="Arial" font-weight="900" font-size="40" text-anchor="middle">
+      <g transform="translate(40, 20)">
+      <rect x="0" y="0" width="120" height="50" fill="none" stroke="#000000" stroke-width="2"/>
+      <text x="60" y="40" font-family="Arial" font-weight="900" font-size="40" text-anchor="middle">
       <tspan fill="#000000">DUDE</tspan>
-    </text>
-  </g>
-</svg>
+      </text>
+      </g>
+      </svg>
 
         
       </main>
