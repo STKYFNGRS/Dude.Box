391324e69ded1d847b26d74a87970c896fe1b663
Alex M <contact@stkyfngrs.com>
2024-11-04 13:37:51 -0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[1fr_auto] min-h-screen items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
+      <main className="flex flex-col gap-8 items-center sm:items-start">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100" width="200" height="100">
           <g transform="translate(40, 20)">
-            <rect x="0" y="0" width="120" height="50" fill="none" stroke="#FFFFFF" strokeWidth="2"/>
+            <rect x="0" y="0" width="120" height="50" fill="none" stroke="#FFFFFF" strokeWidth="2" />
             <text x="60" y="40" fontFamily="Arial" fontWeight="900" fontSize="40" textAnchor="middle">
               <tspan fill="#FFFFFF">DUDE</tspan>
             </text>
           </g>
         </svg>
-        <Analytics/>
       </main>
-      <footer></footer>
+
+      <footer className="row-start-2">
+        <p>
+          &copy; {new Date().getFullYear()} ❤️ dude dot box LLC. All rights reserved.
+        </p>
+      </footer>
+      <Analytics />
     </div>
   );
 }
