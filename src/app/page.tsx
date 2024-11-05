04690558f8d0f4192955f479c0fd1406bebca111
Alex M <contact@stkyfngrs.com>
2024-11-05 08:00:50 -0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,15 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen justify-center p-8 pb-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col items-center">
-        {/* Mobile-friendly "DUDE" box logo */}
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100" className="w-48 h-24">
+        {/* Mobile-styled "DUDE" box logo fixed for all screens */}
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          viewBox="0 0 200 100" 
+          width="200" 
+          height="100" 
+          style={{ maxWidth: "200px", maxHeight: "100px" }} // Ensures fixed size
+          className="absolute-center" // Helper class to center it as per mobile
+        >
           <g transform="translate(40, 20)">
             <rect x="0" y="0" width="120" height="50" fill="none" stroke="#FFFFFF" strokeWidth="2" />
             <text x="60" y="40" fontFamily="Arial" fontWeight="900" fontSize="40" textAnchor="middle">
