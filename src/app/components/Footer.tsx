7c712beb78085c86f65abe80edb4e76acc5a9896
Alex M <contact@stkyfngrs.com>
2024-11-05 13:42:06 -0800

    commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <footer className="w-full bg-gray-800 text-white p-4 mt-auto">
+      <footer className="w-full bg-black text-white p-4 mt-auto">
         <div className="max-w-4xl mx-auto text-center">
         <p className="text-center text-sm text-gray-400">
           Made with ❤️ by dude dot box LLC. &copy; {new Date().getFullYear()}. All rights reserved.
