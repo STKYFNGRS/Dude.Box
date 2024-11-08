38ddf9a8b4b415a327c9a53da10680378550381a
Alex M <contact@stkyfngrs.com>
2024-11-07 17:36:28 -0800

    commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -60,8 +60,8 @@ export default function Home() {
           {/* Founder's Message */}
           <div className="bg-gray-900 p-8 rounded-lg transition duration-300 hover:bg-gray-800 border border-gray-800 shadow-lg">
             <h3 className="text-2xl font-semibold mb-4">From the founder</h3>
-            <div className="flex flex-col md:flex-row gap-6">
-              <div className="relative w-48 h-48 rounded-lg overflow-hidden flex-shrink-0">
+            <div className="flex flex-col md:flex-row gap-6 items-center md:items-center">
+              <div className="relative w-48 h-48 rounded-lg overflow-hidden flex-shrink-0 mx-auto md:mx-0">
                 <Image
                   src="/family2.jpg"
                   alt="Alex Moore with family"
