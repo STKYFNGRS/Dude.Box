dbe9f19fbe2f0da4b4c01c1f97cb348ba9ea0dad
Alex M <contact@stkyfngrs.com>
2024-11-10 20:57:55 -0800

    commit

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN: string;
+        NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN: string;
+      }
+    }
+  }
