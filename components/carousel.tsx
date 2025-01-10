040c2d13422b4579cb9404ae8323d88908d6bfce
Alex M <contact@stkyfngrs.com>
2025-01-10 15:58:24 -0800

    fixed new layout

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -7,32 +7,35 @@ export async function Carousel() {
 
   if (!products?.length) return null;
 
+  // Duplicating products for infinite scroll effect
   const carouselProducts = [...products, ...products, ...products];
 
   return (
-    <div className="w-full overflow-x-auto pb-6 pt-1">
-      <ul className="flex animate-carousel gap-4">
-        {carouselProducts.map((product, i) => (
-          <li
-            key={`${product.handle}${i}`}
-            className="relative aspect-square h-[200px] w-[200px] max-w-[200px] flex-none"
-          >
-            <Link href={`/product/${product.handle}`} className="relative h-full w-full">
-              <GridTileImage
-                alt={product.title}
-                label={{
-                  title: product.title,
-                  amount: product.priceRange.maxVariantPrice.amount,
-                  currencyCode: product.priceRange.maxVariantPrice.currencyCode
-                }}
-                src={product.featuredImage?.url}
-                fill
-                sizes="200px"
-              />
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="flex justify-center">
+      <div className="w-[900px] md:w-[900px] overflow-hidden">
+        <div className="animate-carousel flex gap-4 px-4">
+          {carouselProducts.map((product, i) => (
+            <div
+              key={`${product.handle}${i}`}
+              className="relative aspect-square h-[200px] w-[200px] max-w-[200px] flex-none"
+            >
+              <Link href={`/product/${product.handle}`} className="relative h-full w-full">
+                <GridTileImage
+                  alt={product.title}
+                  label={{
+                    title: product.title,
+                    amount: product.priceRange.maxVariantPrice.amount,
+                    currencyCode: product.priceRange.maxVariantPrice.currencyCode
+                  }}
+                  src={product.featuredImage?.url}
+                  fill
+                  sizes="200px"
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
