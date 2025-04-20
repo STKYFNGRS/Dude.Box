f780e3a5c65698c21aec7f553d44b6f40b9ee145
Alex M <contact@stkyfngrs.com>
2025-04-20 12:52:32 -0700

    Less is more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -186,13 +186,6 @@ export default function Home() {
                     <div className="social-icon-glitch-effect"></div>
                   </a>
                   
-                  <a href="https://x.com/dudedotbox" target="_blank" rel="noopener noreferrer" className="social-icon-container group" aria-label="Follow us on X (Twitter)">
-                    <svg className="w-8 h-8 text-gray-400 group-hover:text-accent transition-colors duration-300" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-                    </svg>
-                    <div className="social-icon-glitch-effect"></div>
-                  </a>
-                  
                   {/* Zora */}
                   <a href="https://zora.co/@dudedotbox" target="_blank" rel="noopener noreferrer" className="social-icon-container group" aria-label="Find us on Zora">
                     <img 
