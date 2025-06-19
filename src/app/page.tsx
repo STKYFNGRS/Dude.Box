d460263701842c6a197083315ad5122c647772b3
Alex M <contact@stkyfngrs.com>
2025-06-19 13:25:13 -0700

    Hello World

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -257,69 +257,32 @@ export default function Home() {
               <div className="flex flex-col items-center gap-6">
                 
                 {/* Social Media Icons */}
-                <div className="flex flex-col items-center gap-4 mt-4"> {/* Changed to flex-col and adjusted gap */}
-                  <div className="flex flex-wrap justify-center gap-6"> {/* First row */}
-                    {/* Discord */}
-                    <a href="https://discord.gg/suvXXNK4n3" target="_blank" rel="noopener noreferrer" className="social-icon-container group" aria-label="Join our Discord">
-                      <svg className="w-6 h-6 text-gray-400 group-hover:text-accent transition-colors duration-300" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z"/>
-                      </svg>
-                      <div className="social-icon-glitch-effect"></div>
-                    </a>
-                    
-                    {/* Zora */}
-                    <a href="https://zora.co/@dudedotbox" target="_blank" rel="noopener noreferrer" className="social-icon-container group" aria-label="Find us on Zora">
-                      <img 
-                        src="/zora.png" 
-                        alt="Zora" 
-                        className="w-6 h-6 object-cover transition-all duration-300" 
-                      />
-                      <div className="social-icon-glitch-effect"></div>
-                    </a>
+                <div className="flex flex-wrap justify-center gap-6 mt-4">
+                  {/* Zora */}
+                  <a href="https://zora.co/@dudedotbox" target="_blank" rel="noopener noreferrer" className="social-icon-container group" aria-label="Find us on Zora">
+                    <img 
+                      src="/zora.png" 
+                      alt="Zora" 
+                      className="w-6 h-6 object-cover transition-all duration-300" 
+                    />
+                    <div className="social-icon-glitch-effect"></div>
+                  </a>
 
-                    {/* Email */}
-                    <a href="mailto:dude@dude.box" className="social-icon-container group" aria-label="Email us">
-                      <svg className="w-6 h-6 text-gray-400 group-hover:text-accent transition-colors duration-300" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M20 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM20 8L12 13L4 8V6L12 11L20 6V8Z"/>
-                      </svg>
-                      <div className="social-icon-glitch-effect"></div>
-                    </a>
+                  {/* Email */}
+                  <a href="mailto:dude@dude.box" className="social-icon-container group" aria-label="Email us">
+                    <svg className="w-6 h-6 text-gray-400 group-hover:text-accent transition-colors duration-300" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M20 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM20 8L12 13L4 8V6L12 11L20 6V8Z"/>
+                    </svg>
+                    <div className="social-icon-glitch-effect"></div>
+                  </a>
 
-                    {/* Facebook */}
-                    <a href="https://www.facebook.com/profile.php?id=61568186610770" target="_blank" rel="noopener noreferrer" className="social-icon-container group" aria-label="Find us on Facebook">
-                      <svg className="w-6 h-6 text-gray-400 group-hover:text-accent transition-colors duration-300" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M22 12.061C22 6.505 17.523 2 12 2S2 6.505 2 12.061c0 5.022 3.657 9.184 8.438 9.939v-7.03h-2.54v-2.91h2.54V9.845c0-2.522 1.492-3.915 3.777-3.915 1.094 0 2.238.197 2.238.197v2.476h-1.26c-1.243 0-1.63.775-1.63 1.57v1.888h2.773l-.443 2.908h-2.33V22c4.78-.755 8.437-4.917 8.437-9.939z"/>
-                      </svg>
-                      <div className="social-icon-glitch-effect"></div>
-                    </a>
-                  </div>
-                  <div className="flex flex-wrap justify-center gap-6"> {/* Second row */}
-                    {/* Instagram */}
-                    <a href="https://www.instagram.com/dudedotbox/" target="_blank" rel="noopener noreferrer" className="social-icon-container group" aria-label="Follow us on Instagram">
-                      <svg className="w-6 h-6 text-gray-400 group-hover:text-accent transition-colors duration-300" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/>
-                      </svg>
-                      <div className="social-icon-glitch-effect"></div>
-                    </a>
-                    
-                    {/* YouTube */}
-                    <a href="https://www.youtube.com/@dudedotbox" target="_blank" rel="noopener noreferrer" className="social-icon-container group" aria-label="Subscribe to our YouTube channel">
-                      <svg className="w-6 h-6 text-gray-400 group-hover:text-accent transition-colors duration-300" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-                      </svg>
-                      <div className="social-icon-glitch-effect"></div>
-                    </a>
-                    
-                    {/* TikTok */}
-                    <a href="https://www.tiktok.com/@dudedotbox" target="_blank" rel="noopener noreferrer" className="social-icon-container group" aria-label="Follow us on TikTok">
-                      <svg className="w-6 h-6 text-gray-400 group-hover:text-accent transition-colors duration-300" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M19.59 6.69a4.83 4.83 0 01-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 01-5.2 1.74 2.89 2.89 0 012.31-4.64 2.93 2.93 0 01.88.13V9.4a6.84 6.84 0 00-1-.05A6.33 6.33 0 005 20.1a6.34 6.34 0 0010.86-4.43v-7a8.16 8.16 0 004.77 1.52v-3.4a4.85 4.85 0 01-1-.1z"/>
-                      </svg>
-                      <div className="social-icon-glitch-effect"></div>
-                    </a>
-                    
-                  </div>
-                  
+                  {/* Threads */}
+                  <a href="https://www.threads.net/@dudedotbox" target="_blank" rel="noopener noreferrer" className="social-icon-container group" aria-label="Follow us on Threads">
+                    <svg className="w-6 h-6 text-gray-400 group-hover:text-accent transition-colors duration-300" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c1.09 0 2.13-.17 3.12-.49-.1-.83-.17-1.7-.17-2.58 0-3.32.88-6.44 2.42-9.14C15.96 7.64 14.08 6 12 6c-3.31 0-6 2.69-6 6s2.69 6 6 6c.34 0 .68-.03 1-.09 0-.61.09-1.2.26-1.78C12.84 16.06 12.43 16 12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4c0 .87-.29 1.67-.78 2.31.49.69.78 1.53.78 2.44 0 .38-.05.75-.14 1.11C17.94 16.54 20 14.47 20 12c0-4.42-3.58-8-8-8z"/>
+                    </svg>
+                    <div className="social-icon-glitch-effect"></div>
+                  </a>
                 </div>
               </div>
           </div>
